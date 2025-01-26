a0d059df2f91c668e8099ebcd10e90d3fc52ee00
Piter Clemente da Silva <piterclementedasilva@gmail.com>
2025-01-26 19:25:17 +0000

    menu_update
    improving the candidate menu.

--- human_resources.py
+++ human_resources.py
@@ -1,9 +1,34 @@
+class ProcessoSeletivo:
+    def __init__(self, nome, idade, sexo, pretSalarial, função):
+        self.nome = nome
+        self.idade = int(idade)
+        self.sexo = sexo
+        self.pretSalarial = int(pretSalarial)   
+        self.função = função
+        self.aprovado = False
+
+    def candidato_aprovado(self): 
+        if not self.aprovado:
+            self.aprovado = True
+            return True
+        return False
+    
+    def candidato_reprovado(self):
+        if self.aprovado:
+            self.aprovado = False
+            return True
+        return False
+    
+    def __str__(self):
+        status = "Aprovado" if self.aprovado else "Reprovado"
+        return f"{self.nome} - {self.função} - [{status}]"
+    
 class Funcionario:
     def __init__(self, nome, idade, sexo, salario, funcao):
         self.nome = nome
-        self.idade = idade
+        self.idade = int(idade)
         self.sexo = sexo
-        self.salario = salario
+        self.salario = int(salario)
         self.funcao = funcao 
         self.admitido = False
 
@@ -30,11 +55,21 @@ class RecursosHumanos:
     def adicionar_funcionarios(self, funcionario):
         self.funcionarios.append(funcionario)
 
+    def adicionar_candidatos(self, candidato):
+        self.funcionarios.append(candidato)
+
     def listar_funcionarios(self):
         if not self.funcionarios:
             print("Nenhum funcionario cadastrado")
         for i, funcionario in enumerate(self.funcionarios, 1):
             print(f'{i}. {funcionario}')
+    
+    def listar_candidatos(self):
+        if not self.funcionarios:
+            print("Nenhum candidato cadastrado")
+        for i, funcionario in enumerate(self.funcionarios, 1):
+            if not funcionario.admitido:
+                print(f'{i}. {funcionario}')
 
     def demitir_funcionarios(self, nome):
         for funcionario in self.funcionarios:
@@ -60,6 +95,10 @@ class RecursosHumanos:
         return False
     
 
+def menu_candidato():
+    print("Menu")
+    print("1. Processo seletivo")
+    print("2. Funcionarios")
 
 def funcionarios_menu():
     print("Menu de funcionarios")
@@ -73,6 +112,32 @@ def main():
     rh = RecursosHumanos()
 
     while True:
+        menu_candidato()
+        escolha = input("Escolha uma opção: ")
+        if escolha == '1':
+            print("Dados Pessoais do Candidto")
+            print("---------------------------")
+            nome = input("Nome:")
+            idade = input("Idade:")
+            sexo = input("Sexo:")
+            pretSalarial = input("Pretensão Salarial:")
+            função = input("Função:")
+            candidato = ProcessoSeletivo(nome, idade, sexo, pretSalarial, função)  
+            rh.adicionar_candidatos(candidato)
+            
+            if candidato.idade <= 18 & candidato.pretSalarial >= 1000:
+                print(f"O candidato {nome} foi reprovado no processo seletivo por ser menor de idade.")
+            else :
+                rh.adicionar_funcionarios(candidato)
+                print(f"O candidato {nome} está sob análise do RH.")
+                break
+                menu_candidato()
+
+        
+        if escolha == '2':
+            funcionarios_menu()
+            break
+
         funcionarios_menu()
         escolha = input("Escolha uma opção: ")
 
@@ -85,7 +150,7 @@ def main():
             funcao = input("Função:")
             funcionario = Funcionario(nome, idade, sexo, salario, funcao)  
             rh.adicionar_funcionarios(funcionario)
-            print(f"O funcionario {nome} foi aprovado  no processo seletivo com sucesso.")
+            print(f"O candidato {nome} foi aprovado  no processo seletivo com sucesso.")
           
 
         elif escolha == '2':    
@@ -99,7 +164,7 @@ def main():
 
         elif escolha == '4':
             print("Lista de funcionarios")     
-            rh.listar_funcionarios()   
+            rh.listar_funcionarios()  
 
         elif escolha == '5':
             break
